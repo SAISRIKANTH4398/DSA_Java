8c14cf0bddf8abf1c1b129602f944c32058b69c6
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-07-14 08:39:19 +0530

    Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+class BinarySearch{
+    public static void main(String[] args){
+        int[] arr = {2,4,6,8,10,11,14,17,23,28,36,45,52};
+        int target = 54;
+        int result = getIndex(arr, target);
+        System.out.println(result);
+    }
+
+    static int getIndex(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target < arr[mid]){
+
+                end = mid - 1;
+
+            }else if(target > arr[mid]){
+
+                start = mid + 1;
+
+            }else{
+
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
