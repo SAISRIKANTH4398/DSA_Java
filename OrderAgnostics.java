ee6e750e4192844d3603de4922a221e6e507c521
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-07-15 12:42:51 +0530

    Order Agnostic Binary Search

--- OrderAgnostics.java
+++ OrderAgnostics.java
@@ -1,4 +1,4 @@
-
+// Order Agnostic method: An Array is sorted in Descending order.
 public class OrderAgnostics {
     public static void main(String[] args){
         int[] arr = {98, 88, 76, 68, 55, 42, 38, 21, 17, 8, 9};
@@ -16,14 +16,14 @@ public class OrderAgnostics {
         while(start<=end){
             int mid = start + (end-start)/2;
 
-            if(arr[mid] < target){
+            if(arr[mid] < target){ //If target is less than mid, array shifts to left side.
                 end = mid - 1;
-            }else if(arr[mid] > target){
+            }else if(arr[mid] > target){ //If target is greater than the mid, array shifts to right side.
                 start = mid + 1;
             }else{
-                return mid;
+                return mid; //If target = mid, returns the index of target value.
             }
         }
-        return -1;
+        return -1; //If target is not in arr, returns -1.
     }
 }
