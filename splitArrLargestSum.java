2a682ff69807eb337eedae16f89f0027b04b5c4b
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-07-17 23:58:01 +0530

    Split Array Largest Sum

---
+++ splitArrLargestSum.java
@@ -0,0 +1,38 @@
+class splitArrLargestSum {
+    public int splitArray(int[] arr, int m){
+        int start = 0;
+        int end = 0;
+
+        for(int i=0; i<=arr.length-1; i++){
+            start = Math.max(start, arr[i]);
+            end += arr[i];
+        }
+
+        if(m==1){
+            return end;
+        }
+
+        while(start < end){
+            int mid = start + (end - start)/2;
+
+            int sum = 0;
+            int pieces = 1;
+
+            for(int num: arr){
+                if(sum + num > mid){
+                    sum = num;
+                    pieces++;
+                }else{
+                    sum+= num;
+                }
+            }
+
+            if(pieces>m){
+                start = mid + 1;
+            }else{
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
