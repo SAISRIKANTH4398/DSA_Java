91711941e0355318bef7a8c05baac7848de68e9a
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-07-20 11:23:10 +0530

    Square root using Binary Search

---
+++ SqrRt.java
@@ -0,0 +1,24 @@
+class SqrRt {
+    public static void main(String[] args){
+
+    }
+
+    int mysqrt(int x){
+        int start = 1;
+        int end = x/2;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(mid == x/mid){
+                return mid;
+            }
+            if(mid < x/mid){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+}
