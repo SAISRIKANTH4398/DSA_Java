b49e0c90641db8a46c180ea8947d869c6fa74360
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-08-27 15:00:52 +0530

    Selection Sort

---
+++ Sorting/Selection.java
@@ -0,0 +1,35 @@
+package Sorting;
+
+import java.util.Arrays;
+
+class Selection {
+    public static void main(String[] args){
+        int[] arr = {5, 3, 4, 2, 1};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr){
+        for(int i=0; i < arr.length; i++){
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    } 
+
+    static int getMaxIndex(int[] arr, int start, int last){
+        int max = start;
+        for(int i=start; i<=last; i++){
+            if(arr[max]<arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+}
